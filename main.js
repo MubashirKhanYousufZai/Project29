3c56f9705e2fb5c627a18e0963bc3863142a800b
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-06-16 15:38:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+class Quaid_e_Azam {
+    Birth() {
+        console.log("He was born on 25 december 1876.");
+    }
+    Education() {
+        console.log("He get is early education in Karachi & Higher education in England");
+    }
+    Politics() {
+        console.log("He struggled for Pakistan");
+    }
+    Governor() {
+        console.log("He was the first the Governor genral of Pakistan And he was the governor genrall untill his death.");
+    }
+    Lakab() {
+        console.log("We called him as FATHER OF NATION.");
+    }
+    Death() {
+        console.log("He was died on 11 September 1948");
+    }
+    Tomb() {
+        console.log("His tomb was in Karachi Pakistan.");
+    }
+}
+let q1 = new Quaid_e_Azam();
+console.log("\t \t \tQuaid-e-Azam\n");
+q1.Birth();
+q1.Education();
+q1.Politics();
+q1.Governor();
+q1.Lakab();
+q1.Death();
+q1.Tomb();
+export {};
